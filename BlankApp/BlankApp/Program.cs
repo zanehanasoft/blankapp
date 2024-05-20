3d6520a10c26efb5dc5221e3bd263017a5f3a565
zanehanasoft <148162727+zanehanasoft@users.noreply.github.com>
2024-05-20 14:20:52 +1000

    Feature1

--- BlankApp/BlankApp/Program.cs
+++ BlankApp/BlankApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +16,32 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapGet("/getuserslastnamelist", (UserDbContext context) =>
+{
+    var allUsers = context.Users.ToList();
+    return allUsers.Select(u => u.LastName).ToList();
+});
+
+app.MapPost("/createuser", (User user, UserDbContext context) =>
+{
+    context.Users.Add(user);
+});
+
 app.UseHttpsRedirection();
 
 app.Run();
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DOB { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+}
+
+public class UserDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+}
